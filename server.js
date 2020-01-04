c39306436987cae5370cef82d1a17cb513b1e037
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-01-04 11:13:16 -0500

    fixed issues with server.js file

--- server.js
+++ server.js
@@ -22,7 +22,7 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 // Define middleware here
@@ -30,13 +30,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/build"));
+  app.use(express.static("client/build"));
 }
 
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, function() {
