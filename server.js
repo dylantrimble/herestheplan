667aabaf4fcc29f5a9b203053ffa2a58d816f496
Rey Diaz <reybdiazjr@gmail.com>
2020-01-05 12:54:18 -0500

    added dependencies needed index.js for models and connect db to server.js and required routes as well.

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
-// const express = require("express")
-// const app = express()
-// const bcrypt = require("bcrypt")
-// const cors = require('cors')
-// const axios = require('axios');
-
-// app.use(cors())
-// app.use(express.json())
-
-// const users = [{ name: "name" }]
-
-// app.get('/users', (req, res) => {
-//     res.json(users)
-// })
-
-// app.post('/users', async (req, res) => {
-
-// })
-
-// app.listen(4440)
-
-const express = require("express");
+const express = require("express")
 const path = require("path");
+const app = express();
+const bcrypt = require("bcrypt")
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const PORT = process.env.PORT || 3002;
-const app = express();
+
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -35,33 +19,6 @@ require("./routes/friends-routes")(app);
 require("./routes/places-routes")(app);
 require("./routes/saved_places-routes")(app);
 require("./routes/users")(app);
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
-const express = require("express")
-const path = require("path");
-const app = express();
-const bcrypt = require("bcrypt")
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-const PORT = process.env.PORT || 3002;
 
 // store users in db
 const users = []
@@ -110,7 +67,11 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
 
