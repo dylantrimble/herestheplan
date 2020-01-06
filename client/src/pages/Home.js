9a409616c21f9c20409914301e9e0f24b0b647f1
Marty McFly <mirko.maya@live.com>
2020-01-05 22:44:12 -0500

    Converted 'Home' to dumb component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,48 +4,50 @@ import Button from "../components/Button/button";
 import SignUpModal from "../components/Signupmodal/signupmodal";
 import SignInModal from "../components/Signinmodal/signinmodal";
 import "../css/main.css";
-class Home extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <Jumbotron>
-          <button
-            type="button"
-            className="btn getStartedBtn"
-            data-toggle="modal"
-            data-target="#exampleModal"
-          >
-            GET STARTED
+
+function Home(props) {
+  console.log(props.newUser)
+  return (
+    <div className="wrapper">
+      <Jumbotron>
+        <button
+          type="button"
+          className="btn getStartedBtn"
+          data-toggle="modal"
+          data-target="#exampleModal"
+        >
+          GET STARTED
           </button>
-        </Jumbotron>
-        <SignUpModal>
-          <a
-            type="button"
-            data-dismiss="modal"
-            aria-label="Close"
-            className="btn signUpInBtn"
-            data-toggle="modal"
-            data-target="#signInModal"
-          >
-            Sign In
+      </Jumbotron>
+      <SignUpModal
+        newUser={props.newUser}
+      >
+        <a
+          type="button"
+          data-dismiss="modal"
+          aria-label="Close"
+          className="btn signUpInBtn"
+          data-toggle="modal"
+          data-target="#signInModal"
+        >
+          Sign In
           </a>
-          <Button className="submitBtn">Submit</Button>
-        </SignUpModal>
-        <SignInModal>
-          <a
-            type="button"
-            data-dismiss="modal"
-            aria-label="Close"
-            className="btn signUpInBtn"
-            data-toggle="modal"
-            data-target="#exampleModal"
-          >
-            Sign Up
+        <Button className="submitBtn">Submit</Button>
+      </SignUpModal>
+      <SignInModal>
+        <a
+          type="button"
+          data-dismiss="modal"
+          aria-label="Close"
+          className="btn signUpInBtn"
+          data-toggle="modal"
+          data-target="#exampleModal"
+        >
+          Sign Up
           </a>
-          <Button className="submitBtn">Submit</Button>
-        </SignInModal>
-      </div>
-    );
-  }
+        <Button className="submitBtn">Submit</Button>
+      </SignInModal>
+    </div>
+  );
 }
 export default Home;
