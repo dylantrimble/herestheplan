753f57aa91face2a421e5cd2b03758eeece6d679
Marty McFly <mirko.maya@live.com>
2020-01-06 21:56:54 -0500

    Testing api.post for newUser

--- routes/users.js
+++ routes/users.js
@@ -26,6 +26,49 @@ module.exports = function(app) {
     });
   });
 
+  app.post("/api/newUsers", async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      const newUser = { 
+        fullName: req.body.fullName,
+        username: req.body.username,
+        password: hashedPassword 
+      }
+      users.push(user)
+      res.status(201).send("User added.")
+    } catch {
+      res.status(500).send()
+    }
+  })
+
+  /////////////////////////
+
+  app.post("/api/newUsers", function (req, res) {
+
+    console.log(req.body)
+
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(req.body.password, salt, function(err, hash) {
+
+        newUser.save(function(err) {
+          if(!err) {
+            return res.send({ status: 'User created' });
+          } else {
+            if(err.name == 'ValidationError') {
+              res.statusCode = 400;
+              res.send({ error: 'Bad Request' });
+            } else {
+              res.statusCode = 500;
+              res.send({ error: 'Internal Server Error' });
+            }
+          } 
+        });
+      });
+    });
+  });
+
+  /////////////////////////
+
   app.delete("/api/users/:id", function(req, res) {
     db.User.destroy({
       where: {
